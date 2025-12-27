cc59ab901fb7f5af0f9f80b2510f0e9d0223d3bb
orkEnable <ork.develop@gmail.com>
2025-12-27 18:37:48 +0900

    add:メモのエラーハンドリングを追加

--- backend/src/modules/memo/infra/prisma-memo.repository.ts
+++ backend/src/modules/memo/infra/prisma-memo.repository.ts
@@ -2,36 +2,75 @@ import { Injectable } from '@nestjs/common';
 import { MemoRepository } from '../domain/memo.repository';
 import { Memo } from '../domain/entities/memo.entity';
 import { PrismaService } from '../../../../prisma/prisma.service';
+import {
+  RepositoryConflictError,
+  RepositoryNotFoundError,
+  RepositoryPersistenceError,
+} from '../../common/errors';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class PrismaMemoRepository implements MemoRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(memo: Memo): Promise<void> {
-    await this.prisma.memo.create({
-      data: {
-        id: memo.id,
-        userId: memo.userId,
-        title: memo.title,
-        content: memo.content,
-      },
-    });
+    try {
+      await this.prisma.memo.create({
+        data: {
+          id: memo.id,
+          userId: memo.userId,
+          title: memo.title,
+          content: memo.content,
+        },
+      });
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2002'
+      ) {
+        throw new RepositoryConflictError(
+          '同じidのメモがすでに登録されています。',
+          e,
+        );
+      }
+      throw new RepositoryPersistenceError('メモの作成に失敗しました。', e);
+    }
   }
 
   async update(memo: Memo): Promise<void> {
-    await this.prisma.memo.update({
-      where: { id: memo.id },
-      data: {
-        title: memo.title,
-        content: memo.content,
-      },
-    });
+    try {
+      await this.prisma.memo.update({
+        where: { id: memo.id },
+        data: {
+          title: memo.title,
+          content: memo.content,
+        },
+      });
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
+      ) {
+        throw new RepositoryNotFoundError('メモが見つかりません', e);
+      }
+      throw new RepositoryPersistenceError('メモの更新に失敗しました', e);
+    }
   }
 
   async delete(id: string): Promise<void> {
-    await this.prisma.memo.delete({
-      where: { id },
-    });
+    try {
+      await this.prisma.memo.delete({
+        where: { id },
+      });
+    } catch (e) {
+      if (
+        e instanceof Prisma.PrismaClientKnownRequestError &&
+        e.code === 'P2025'
+      ) {
+        throw new RepositoryNotFoundError('メモが見つかりません', e);
+      }
+      throw new RepositoryPersistenceError('メモの削除に失敗しました', e);
+    }
   }
 
   async findById(id: string): Promise<Memo | null> {
