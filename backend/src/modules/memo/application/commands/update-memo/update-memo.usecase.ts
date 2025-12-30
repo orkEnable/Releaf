6faa7ba2f2c6346ffcbdbfd4bbe9f428cba31be3
orkEnable <ork.develop@gmail.com>
2025-12-30 17:42:45 +0900

    fix: 引数型への変更と戻り値の追加

--- backend/src/modules/memo/application/commands/update-memo/update-memo.usecase.ts
+++ backend/src/modules/memo/application/commands/update-memo/update-memo.usecase.ts
@@ -4,11 +4,12 @@ import { MemoNotFoundApplicationError } from '../../errors/memo-not-found.error'
 
 export class UpdateMemoUsecase {
   constructor(private readonly memoRepository: MemoRepository) {}
-  async execute(command: UpdateMemoCommand) {
+  async execute(command: UpdateMemoCommand): Promise<void> {
     const memo = await this.memoRepository.findById(command.memoId);
     if (!memo) {
       throw new MemoNotFoundApplicationError(command.memoId);
     }
+    memo.update(command.title, command.content);
     await this.memoRepository.update(memo);
   }
 }
