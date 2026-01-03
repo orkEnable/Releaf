38af3a18c0bedf6c39719149f1b6731f8ee0abf9
orkEnable <ork.develop@gmail.com>
2026-01-03 20:23:31 +0900

    add: ExecutionContextを使用するように修正

--- backend/src/auth/current-user.decorator.ts
+++ backend/src/auth/current-user.decorator.ts
@@ -1,12 +1,11 @@
-import { createParamDecorator } from '@nestjs/common';
-import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export type CurrentUser = {
   userId: string;
 };
 
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContextHost): CurrentUser => {
+  (_data: unknown, ctx: ExecutionContext): CurrentUser => {
     const request = ctx.switchToHttp().getRequest<{ user: CurrentUser }>();
     return request.user;
   },
